e4d3fbb8015ed06665e6f3715e9180face59ff0b
Vitor Paulon <vtrpaulon@gmail.com>
2023-03-14 18:23:19 +0000

    Aprendendo variaveis

---
+++ Variaveis.java
@@ -0,0 +1,16 @@
+public class Variaveis {
+
+    public static void main (String[] args){
+
+        String nome = "Vitor";
+        int idade = 35;
+        String cpf = "4455566677";
+        float altura = 1.76f;
+
+
+        System.out.println("Ola "+nome);
+        System.out.println("Voce tem "+idade+" anos");
+        System.out.println("Seu cpf é "+cpf);
+        System.out.println("Sua altura é "+altura);
+    }
+}
